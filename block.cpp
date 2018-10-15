cb946338d086e9219f2306bdbdcacb394913dbdf
Evan Azevedo <eazevedo@csil.cs.ucsb.edu>
2018-10-14 23:42:26 -0700

    all initial files

---
+++ block.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int nrow, ncol;
+	string prompt_inp = "Enter number of rows and columns:\n";
+	cout << prompt_inp;
+	cin >> nrow >> " " >> ncol >> endl;
+	while(i <= ncol){
+		for(j
+	}
+}
